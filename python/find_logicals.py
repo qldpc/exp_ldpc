6188586295c4bba4bd538aab6ee6bf9e76717047
Chris Pattison <chpattison@gmail.com>
2021-11-10 23:52:07 -0800

    Find logicals bugfixes and test for d=3 rotated surface code

--- python/find_logicals.py
+++ python/find_logicals.py
@@ -13,12 +13,12 @@ def matrix_to_module_elements(matrix, basis):
 
 def sparse_to_support(sparse_matrix : sparse.spmatrix):# -> Iterable[Set[int]]:
     '''Convert a scipy sparse matrix to a iter of sets indicating non-zero entries in each row'''
-    return (set(col for (_, col, _) in sparse.find(sparse_matrix[row_index, :])) for row_index in range(num_rows(sparse_matrix)))
+    return (frozenset(sparse.find(sparse_matrix[row_index, :])[1]) for row_index in range(num_rows(sparse_matrix)))
 
 def sparse_to_f2_matrix(sparse_matrix : sparse.spmatrix):
     '''Convert a scipy sparse matrix to a dense GF(2) matrix'''
     def make_row(entries):
-        return [1 if i in entries else 0 for i in range(num_cols(entries))]
+        return [1 if i in entries else 0 for i in range(num_cols(sparse_matrix))]
     return Matrix(GF(2), map(make_row, sparse_to_support(sparse_matrix)))
 
 
@@ -37,17 +37,20 @@ def find_logicals(checks : QuantumCodeChecks) -> QuantumCodeLogicals:
     def find_logical_single_basis(checks, conjugate_checks):
         # Find the kernel of the check matrix
         kernel = get_kernel(sparse_to_f2_matrix(checks))
-        
-        # Reduce the kernel mod the image of the boundary operator (transpose of conjugate check matrix)
-        conjugate_submodule = module.submodule(matrix_to_module_elements(conjugate_checks, module_basis))
-        return make_check_matrix(set(g.support() for g in set(conjugate_submodule.reduce(g) for g in matrix_to_module_elements(kernel, module_basis)) if len(g.monomials()) > 0))
 
-    return (find_logical_single_basis(x_checks, z_checks), find_logical_single_basis(z_checks, x_checks), num_qubits)
+        # Reduce the kernel mod the image of the boundary operator (transpose of conjugate check matrix)
+        conjugate_submodule = module.submodule(matrix_to_module_elements(sparse_to_f2_matrix(conjugate_checks), module_basis))
+        logical_support = (frozenset(g.support()) for g in frozenset(conjugate_submodule.reduce(g) for g in matrix_to_module_elements(kernel, module_basis)) if len(g.monomials()) > 0)
+        return make_check_matrix(list(logical_support), num_qubits)
+
+    return (find_logical_single_basis(z_checks, x_checks), find_logical_single_basis(x_checks, z_checks), num_qubits)
 
 def test_find_logicals():
     from .code_examples import d3_rotated_surface_code
     checks = d3_rotated_surface_code()
     logicals = find_logicals(checks)
+    assert list(sparse_to_support(logicals[0])) == [{8, 6, 7}]
+    assert list(sparse_to_support(logicals[1])) == [{8, 2, 5}]
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
