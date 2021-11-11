f90adcb46d7a292bf3d16059a159f4a215885877
Chris Pattison <chpattison@gmail.com>
2021-11-10 18:18:55 -0800

    Fix make_check_matrix

--- python/qecc_util.py
+++ python/qecc_util.py
@@ -16,5 +16,5 @@ def num_cols(a : np.array) -> int:
 
 def make_check_matrix(checks : Iterable[Iterable[int]], qubit_count) -> sparse.csr_matrix:
     '''Given check matrix specified as non-zero entries, construct a scipy sparse matrix'''
-    coo_entries = np.array((row_index, v, 1) for (row_index, row) in enumerate(checks) for v in row)
+    coo_entries = np.array([[row_index, v, 1] for (row_index, row) in enumerate(checks) for v in row])
     return sparse.csr_matrix((coo_entries[:,2], (coo_entries[:,0], coo_entries[:,1])), shape=(len(checks), qubit_count))
