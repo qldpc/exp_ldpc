2d15c4b2750de92b4fd83de014430b4d1d5fc498
Chris Pattison <chpattison@gmail.com>
2022-02-08 19:53:26 -0600

    Properly create random biregular graphs

--- qldpc/hypergraph_product_code.py
+++ qldpc/hypergraph_product_code.py
@@ -2,9 +2,9 @@ from .homological_product_code import homological_product
 import networkx as nx
 import numpy as np
 from .qecc_util import QuantumCodeChecks, QuantumCodeLogicals, num_cols, num_rows
+from .random_biregular_graph import random_biregular_graph
 
-
-def biregular_hpg(num_data : int, data_degree : int, check_degree : int, seed=None) -> (QuantumCodeChecks, QuantumCodeLogicals):
+def biregular_hpg(num_data : int, data_degree : int, check_degree : int, seed=None, graph_multiedge_retries=None) -> (QuantumCodeChecks, QuantumCodeLogicals):
     ''' Constructs a hypergraph product code defined by a single (data_degree, check_degree)-regular bipartite graph
         In the classical code, the check nodes represent a basis of 1-chains and the data nodes represent a basis of 0-chains.
         The boundary map from 1-chains to 0-chains gives the neighborhood of data nodes.
@@ -12,23 +12,7 @@ def biregular_hpg(num_data : int, data_degree : int, check_degree : int, seed=No
     '''
 
     num_checks = (num_data * data_degree)//check_degree
-    if num_checks*check_degree != num_data*data_degree:
-        raise RuntimeError('Number of data bits incompatible with data and check degrees')
-
-    # Generate classical Tanner graph
-    tanner_graph = nx.bipartite.configuration_model(
-        [data_degree for _ in range(num_data)], # Data marked with weight 0
-        [check_degree for _ in range(num_checks)], # Checks marked with weight 1
-        create_using=nx.Graph(),
-        seed=seed,
-        )
-
-    # Consistency check
-    for (node, degree) in tanner_graph.degree():
-        if tanner_graph.nodes[node]['bipartite'] == 0:
-            assert degree == data_degree
-        else:
-            assert degree == check_degree
+    tanner_graph = random_biregular_graph(num_checks, num_data, data_degree, check_degree, seed=seed, graph_multiedge_retries=graph_multiedge_retries)
     
     boundary_map = nx.bipartite.biadjacency_matrix(tanner_graph, row_order=[v for v in tanner_graph.nodes if tanner_graph.nodes[v]['bipartite'] == 0]).astype(int)
     # Why aren't the checks overcomplete here?
@@ -44,7 +28,7 @@ def biregular_hpg(num_data : int, data_degree : int, check_degree : int, seed=No
     return ((x_checks, z_checks, num_qubits), (x_logicals, z_logicals, num_qubits))
 
 def random_test_hpg() -> (QuantumCodeChecks, QuantumCodeLogicals):
-    return biregular_hpg(36, 3, 4, seed=670235982)
+    return biregular_hpg(36, 3, 4, seed=42)
 
 def test_smoketest_biregular_hpg():
     ((x_checks, z_checks, _), (x_logicals, z_logicals, _)) = random_test_hpg()
---
+++ qldpc/random_biregular_graph.py
@@ -0,0 +1,86 @@
+import networkx as nx
+import numpy as np
+import itertools
+
+def random_biregular_graph(num_checks : int, num_data : int, data_degree : int, check_degree : int, seed=None, graph_multiedge_retries=None):
+    if graph_multiedge_retries is None:
+        graph_multiedge_retries = 100
+
+    if num_checks*check_degree != num_data*data_degree:
+        raise RuntimeError('Number of data bits incompatible with data and check degrees')
+
+    # Generate classical Tanner graph
+    tanner_graph = nx.bipartite.configuration_model(
+        [data_degree for _ in range(num_data)], # Data marked with weight 0
+        [check_degree for _ in range(num_checks)], # Checks marked with weight 1
+        seed=seed,
+        create_using=nx.MultiGraph(),
+        )
+
+    # Swap endpoints of edges s.t. we have no multiedges
+    # Swapping ensures that we still have a biregular graph at the end
+    # It is possible that this procedure creates multiedges because we do not enforce that (a, b') does not already exist
+
+    #  a  --- b          a   b
+    #                     \ / 
+    #             |->      X    
+    #                     / \ 
+    #  a' --- b'         a'  b'
+
+    rng = np.random.default_rng(seed=seed)
+    data_vertices = [v for v, d in tanner_graph.nodes(data=True) if d['bipartite'] == 0]
+    for _ in range(graph_multiedge_retries):
+        multiedge_list = []
+        # Create list of multiedges
+        for node in data_vertices:
+            for neighbor_node in tanner_graph.neighbors(node):
+                num_edges = tanner_graph.number_of_edges(node, neighbor_node)
+                # (node, neighbor_node) has a multiedge
+                if num_edges > 1:
+                    multiedge_list.extend(itertools.repeat((node, neighbor_node), num_edges-1))
+    
+        print(len(multiedge_list))
+        if len(multiedge_list) == 0:
+            break
+        
+        # ---------------
+
+        edge_removal_list = []
+        edge_add_list = []
+
+        # Compute update
+        edge_list = list(tanner_graph.edges())
+        swap_edges = rng.choice(edge_list, size=len(multiedge_list), replace=False)
+        for (edge_a, edge_b) in zip(multiedge_list, swap_edges):
+            new_edge_a = (edge_a[0], edge_b[1])
+            new_edge_b = (edge_b[0], edge_a[1])
+
+            edge_removal_list.append(edge_a)
+            edge_removal_list.append(edge_b)
+
+            edge_add_list.append(new_edge_a)
+            edge_add_list.append(new_edge_b)
+        
+        # Apply update
+        for e in edge_removal_list:
+            # Removes an arbitrary edge if there are multiple edges like e
+            tanner_graph.remove_edge(*e, key=None)
+        tanner_graph.add_edges_from(edge_add_list)
+    else:
+        raise RuntimeError('Unable to remove multiedges from the graph')
+
+    tanner_graph = nx.Graph(tanner_graph)
+
+    return tanner_graph
+        
+def check_biregular(G, data_degree, check_degree):
+    # Consistency check
+    for (node, degree) in G.degree():
+        if G.nodes[node]['bipartite'] == 0:
+            assert degree == data_degree
+        else:
+            assert degree == check_degree
+
+def test_smoketest_random_biregular_graph():
+    graph = random_biregular_graph(27, 36, 3, 4, seed=42)
+    check_biregular(graph, 3, 4)
