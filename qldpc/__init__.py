777d662d0be99517a3341d208884e0d9f91e4047
Chris Pattison <chpattison@gmail.com>
2022-05-06 16:48:41 -0700

    Guard qldpc library import so we don't have to build to run python tests

--- qldpc/__init__.py
+++ qldpc/__init__.py
@@ -1,4 +1,10 @@
 from .storage_sim import depolarizing_noise_model, build_storage_simulation
 from .hypergraph_product_code import biregular_hpg
 from .quantum_code_io import read_check_generators, write_check_generators
-from .qldpc import ErrorCorrectingCode, FirstMinBeliefProp, SmallSetFlip, FirstMinBPplusSSF
+
+# Import decoders but don't blow up if we want to use some of the other routines without compiling the decoder library
+import warnings
+try:
+    from .qldpc import ErrorCorrectingCode, FirstMinBeliefProp, SmallSetFlip, FirstMinBPplusSSF
+except ModuleNotFoundError:
+    warnings.warn('Unable to import qldpc decoder library')
