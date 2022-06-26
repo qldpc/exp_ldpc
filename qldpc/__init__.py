af66507b8a5961fd1f5b92d50e49e560de0c6615
Chris Pattison <chpattison@gmail.com>
2022-06-25 17:46:35 -0700

    Add offline permutation routing utility

--- qldpc/__init__.py
+++ qldpc/__init__.py
@@ -1,6 +1,7 @@
 from .storage_sim import depolarizing_noise_model, build_storage_simulation
 from .hypergraph_product_code import biregular_hpg
 from .quantum_code_io import read_check_generators, write_check_generators
+from .swap_route import grid_permutation_route
 
 # Import decoders but don't blow up if we want to use some of the other routines without compiling the decoder library
 import warnings
