8e88e9dedecc1c113d9d0935c077634724d98251
Chris Pattison <chpattison@gmail.com>
2022-07-19 15:42:35 -0700

    LP Code WIP

--- qldpc/lifted_product_code.py
+++ qldpc/lifted_product_code.py
@@ -17,28 +17,33 @@ from abc import ABC, abstractmethod
 from galois import GF, FieldArray
 
 class Group(ABC):
-    @classmethod
     @abstractmethod
-    def __mul__(self, other: Group) -> Group:
+    def __matmul__(self, other: Group) -> Group:
         pass
 
-    @classmethod
     @abstractmethod
     def inv(self) -> Group:
         pass
 
-    @staticmethod
+    @abstractmethod
     def identity(self) -> Group:
         pass
+
+    @abstractmethod
+    def __hash__(self):
+        pass
     
-    @classmethod
-    def pow(self, x:int) -> Group:
-        '''Return g**x where x in {0,1} i.e. identity or x'''
-        assert x == 0 or x == 1
-        return self if x == 1 else self.identity()
+    def __pow__(self, n:int) -> Group:
+        '''Return g**n where n is a positive integer'''
+        assert n is int
+        assert n >= 0
+        r = self.identity()
+        for _ in range(n):
+            r = r * self
+        return r
 
 # @dataclass(frozen=True)
-class PGL2(Group):
+class GL2(Group):
     _gf : FieldArray
     data : FieldArray
 
@@ -46,19 +51,29 @@ class PGL2(Group):
         super().__init__()
         self._gf = gf
         self.data = gf(data)
+        self.data.flags.writeable = False
 
-    @classmethod
-    def __mul__(self, other: PGL2) -> PGL2:
+    def __matmul__(self, other: PGL2) -> PGL2:
         return type(self)(self._gf, self.data @ other.data)
 
-    @classmethod
     def inv(self) -> PGL2:
         return type(self)(self._gf, np.linalg.inv(self.data))
 
-    @staticmethod
-    def identity() -> PGL2:
-        return self._gf.Identity(2)
+    def identity(self) -> PGL2:
+        return type(self)(self._gf, self._gf.Identity(2))
 
+    def __hash__(self):
+        return hash((self._gf.order, self.data.tobytes()))
+
+    def __eq__(self, other):
+        return (self._gf.order == other._gf.order) and np.all(self.data == other.data)
+
+    def __repr__(self):
+        return repr(self.data)
+    
+class PGL2(GL2):
+    pass
+    
 @dataclass(frozen=True)
 class EdgeEdge:
     e: int
@@ -88,8 +103,8 @@ def morgenstern_generators(l, i) -> List[PGL2]:
     This follows the overview in Dinur et al. (2021) arXiv:2111.04808
     '''
     assert l >= 1
-    # This restriction is required by the text
-    assert l % 2 == 0
+    # # This restriction is required by the text
+    # assert l % 2 == 0
     q = 2**l
     Fq = GF(q)
     Fqi = GF(q**i)
@@ -110,9 +125,33 @@ def morgenstern_generators(l, i) -> List[PGL2]:
     generators = [PGL2(Fqi, [[1, (g+d*i_element)],[x*(g+d+d*i_element), 1]]) for (g,d) in pairs]
     return generators
 
+def _dfs_generators(traverse, root, generators):
+    visited = set()
+    frontier = deque([root])
+    while True:
+        try:
+            # Grab the next leaf on the frontier
+            leaf = frontier.pop()
+            if leaf in visited:
+                continue
+            visited.add(leaf)
+        except IndexError:
+            break
+        # Compute the new fronter
+        for g in generators:
+            frontier.append(traverse(leaf, g))
+            
+    return visited
+
 def test_morgenstern_generators():
-    morgenstern_generators(2,4)
-    pass
+    l = 1
+    i = 2
+    generators = morgenstern_generators(l,i)
+    identity = generators[0].identity()
+
+    group_elements = _dfs_generators(lambda a,b: a@b, identity, generators)
+    q = (2**l)**i
+    assert len(group_elements) == (q-1)*q*(q+1)
     # Do DFS using the generators from the left and from the right to make sure we get the number of elements we expect
     # Check a \in A implies a^-1 \in A
 
@@ -144,13 +183,14 @@ def lifted_product_code(group : List[Group], gen : List[Group], h1, h2, check_co
         x_check = deque()
         # ExV -> VxV
         # Remembering the local system for E -> V
+        # v1 is either 0 or 1 so we abuse notation and raise the shift to it
         x_check.extend(
-            VertexVertex((v1, r1), gen(e1).pow(v1) * g, (v2, r2))
+            VertexVertex((v1, r1), gen(e1)**v1 * g, (v2, r2))
             for r1 in h1_system for v1 in vertices if h1[r1,e1] != 0)
 
         # ExV -> ExE
         x_check.extend(
-            EdgeEdge(e1, g * gen(e2).inv().pow(v2), e2)
+            EdgeEdge(e1, g * gen(e2).inv()**v2, e2)
             for e2 in edges if h2[r2,e2] != 0)
 
         x_supports[EdgeVertex(e1,g,(v2,r2))] = x_check
@@ -159,14 +199,14 @@ def lifted_product_code(group : List[Group], gen : List[Group], h1, h2, check_co
     q_supports = dict()
     for (e1, g, e2) in product(edges, group, edges):
         support = deque()
-        support.extend( VertexEdge((v1, r1), gen(e1).pow(v1)*g, e2)
+        support.extend( VertexEdge((v1, r1), gen(e1)**v1 *g, e2)
             for r1 in h1_system if h1[h1_system, e1] != 0 for v1 in vertices)
 
         q_supports[EdgeEdge(e1,g,e2)] = support
 
     for (v1, r1, g, v2, r2) in product(vertices, h1_system, group, vertices, h2_system):
         support = deque()
-        support.extend( VertexEdge((v1, r1), g*gen(e2).inv().pow(v2), e2) for e2 in edges if h2[r2,e2] != 0)
+        support.extend( VertexEdge((v1, r1), g*gen(e2).inv()**v2, e2) for e2 in edges if h2[r2,e2] != 0)
         
         q_supports[VertexVertex((v1, r1), g, (v2, r2))] = support
 
