748ef1e5eaafc679b66da79cd5a323c07fa83475
Chris Pattison <chpattison@gmail.com>
2022-08-27 19:28:28 -0400

    Make internal calls for O(n^3) matrix operations

--- python/qldpc/linalg.py
+++ python/qldpc/linalg.py
@@ -89,11 +89,21 @@ from typing import List
 
 #     return (D, S, T)
 
+def gf2_null_space(A : np.array) -> np.array:
+    return GF2(A).null_space()
+
+def gf2_column_space(A : np.array) -> np.array:
+    return GF2(A).column_space()
+
+def gf2_row_reduce(A : np.array, ncols : int = None) -> np.array:
+    return GF2(A).row_reduce(ncols=ncols)
 
 def gf2_get_pivots(A : np.array) -> List[int]:
     largest_index = (A!=0).argmax(axis=1)
     return np.extract(A[range(A.shape[0]), largest_index]!=0, largest_index)
 
+def gf2_matrix_rank(A : np.array) -> int:
+    return get_rank(A)
 
 def get_rank(A : np.array) -> int:
     return np.linalg.matrix_rank(GF2(A))
