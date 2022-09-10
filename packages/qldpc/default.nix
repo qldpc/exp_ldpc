e1c9155c10b30edb27522c5d7acf16edd11bb1de
Chris Pattison <chpattison@gmail.com>
2022-09-09 17:04:09 -0700

    minor version bump v0.9.0

--- packages/qldpc/default.nix
+++ packages/qldpc/default.nix
@@ -12,7 +12,7 @@
 
 buildPythonPackage rec {
   pname = "qldpc";
-  version = "0.8.3";
+  version = "0.9.0";
   format = "pyproject";
   src = ../../.;
   nativeBuildInputs = with rustPlatform; [ cargoSetupHook maturinBuildHook ];
@@ -20,7 +20,7 @@ buildPythonPackage rec {
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
     name = "${pname}-${version}";
-    hash = "sha256-LMlxAmsdWQWl62MrQq2TOX6nfpBUd+uykEKay1Y5k/o=";
+    hash = "sha256-PEaz36nF4BIEySpgjtiEh965DiaoS+klYtAIjvjOcac=";
   };
 
   buildInputs = lib.optionals stdenv.isDarwin [ libiconv ];
