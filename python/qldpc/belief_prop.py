fc1b575a837a6a6dc3210410aa4726a3f49109a3
Chris Pattison <chpattison@gmail.com>
2022-10-08 02:33:02 -0700

    Robust BP decoder

--- python/qldpc/belief_prop.py
+++ python/qldpc/belief_prop.py
@@ -4,9 +4,18 @@ from numba import int32, float64
 from typing import Dict
 from array import array
 
+def _log1pexp(x):
+    '''Compute Log[1+Exp[x]] without overflowing in the exp.
+    The discontinuity at 12e is about 6.8e-15'''
+    assert not np.isnan(x)
+    
+    transition = 12*np.e
+    return x if x > transition else np.log1p(np.exp(x))
+
 class BeliefPropagation:
     _check_to_bit : Dict
     _bit_to_check : Dict
+    _check_matrix : sparse.csr_matrix
     
     def __init__(self, check_matrix : sparse.spmatrix):
         self._check_to_bit = {i:array('L') for i in range(check_matrix.shape[0])}
@@ -15,8 +24,10 @@ class BeliefPropagation:
         for i,j in zip(*check_matrix.nonzero()):
             self._check_to_bit[i].append(j)
             self._bit_to_check[j].append(i)
+        
+        self._check_matrix = check_matrix.tocsr()
+    
 
-            
     @staticmethod
     def llr_sum(a_llr, b_llr):
         '''Numerically stable way to compute the llr of (a + b). Equation (6) of Chen et al. IEEE Trans. Comm. 53 (8) 1288-1299 (2005)'''
@@ -25,20 +36,30 @@ class BeliefPropagation:
         
         return (
             np.sign(a_llr) * np.sign(b_llr) * np.minimum(np.abs(a_llr), np.abs(b_llr))
-            + np.log1p(np.exp(ab_sum))
-            - np.log1p(np.exp(ab_diff))
+            + _log1pexp(ab_sum)
+            - _log1pexp(ab_diff)
         )
+
+    def check_converged(self, llr, syndrome):
+        return np.all((self._check_matrix @ np.where(llr > 0, 1, 0).astype(np.uint32))%2 == syndrome)
 
     def decode(self, syndrome, llr_prior, iterations, harden=None):
         if harden is None:
             harden = True
         
         syndrome_R = np.where(syndrome == 0, 1, -1)
+        # We can allocate this as a skinny matrix with row/col indices given by local coordinates instead of global ones
+        # Take max local system size to make everything convenient
         messages_v_to_c = np.zeros((len(syndrome), len(llr_prior)))
         messages_c_to_v = np.zeros((len(syndrome), len(llr_prior)))
         
         llr = llr_prior
         for _ in range(iterations):
+            # v -> c
+            for bit, check_list in self._bit_to_check.items():
+                for check in check_list:
+                    messages_v_to_c[check,bit] = llr[bit] - messages_c_to_v[check, bit]
+
             # c -> v
             for check, bit_list in self._check_to_bit.items():
                 # We need to handle the case where check_list has 1 element
@@ -56,23 +77,18 @@ class BeliefPropagation:
                     b_llr[i] = self.llr_sum(b_llr[i+1], messages_v_to_c[check, bit_list[i]])
 
                 # Compute messages
-                messages_c_to_v[check, bit_list[0]] = b_llr[1]
-                messages_c_to_v[check, bit_list[-1]] = f_llr[-2]
+                messages_c_to_v[check, bit_list[0]] = syndrome_R[check]*b_llr[1]
+                messages_c_to_v[check, bit_list[-1]] = syndrome_R[check]*f_llr[-2]
                 for i in range(2, len(bit_list)-1):
-                    messages_c_to_v[check, bit_list[i]] = self.llr_sum(f_llr[i-1], b_llr[i+1])
+                    messages_c_to_v[check, bit_list[i]] = syndrome_R[check]*self.llr_sum(f_llr[i-1], b_llr[i+1])
                     
-            print(f'{messages_c_to_v=}')
 
             # LLRs
-            llr = llr_prior
+            llr = np.copy(llr_prior)
             for bit, check_list in self._bit_to_check.items():
-                llr[bit] = np.sum(messages_c_to_v[c, bit] for c in check_list)
+                llr[bit] += sum(messages_c_to_v[c, bit] for c in check_list)
 
-            # v -> c
-            for bit, check_list in self._bit_to_check.items():
-                for check in check_list:
-                    messages_v_to_c[check,bit] = llr[bit] - messages_c_to_v[check, bit]
-
-            print(llr)
+            if self.check_converged(llr, syndrome):
+                break
                     
-        return np.where(llr < 0, 1, 0).astype(np.uint32) if harden else llr
+        return np.where(llr > 0, 1, 0).astype(np.uint32) if harden else llr
--- tests/test_belief_prop.py
+++ tests/test_belief_prop.py
@@ -2,16 +2,43 @@ from qldpc import BeliefPropagation
 import numpy as np
 import scipy.sparse as sparse
 
+# A small LDPC code from MacKay's website
+# 96.3.963 (N=96,K=48,M=48,R=0.5)
+_checks =  [[10, 30, 40], [5, 32, 45], [16, 18, 39], [12, 22, 38], [15, 19, 47], [2, 17, 34], [9, 24, 42], 
+    [1, 29, 33], [4, 27, 36], [3, 26, 35], [11, 31, 43], [7, 21, 44], [8, 20, 48], [14, 23, 46], 
+    [6, 28, 37], [13, 25, 41], [14, 32, 43], [5, 23, 37], [2, 31, 36], [1, 28, 34], [7, 25, 47], 
+    [10, 21, 33], [15, 30, 35], [16, 26, 48], [3, 22, 46], [12, 20, 41], [8, 18, 38], [4, 19, 45], 
+    [6, 24, 40], [9, 27, 39], [13, 17, 42], [11, 29, 44], [8, 24, 34], [6, 25, 36], [9, 19, 43], 
+    [1, 20, 46], [14, 27, 42], [7, 22, 39], [13, 18, 35], [4, 26, 40], [16, 29, 38], [15, 21, 48], 
+    [11, 23, 45], [3, 17, 47], [5, 28, 44], [12, 32, 33], [2, 30, 41], [10, 31, 37], [10, 18, 36], 
+    [4, 23, 44], [9, 29, 40], [2, 27, 38], [8, 30, 42], [12, 28, 43], [11, 20, 37], [1, 19, 35], 
+    [15, 31, 39], [16, 32, 41], [5, 26, 33], [3, 25, 45], [13, 21, 34], [14, 24, 48], [7, 17, 46], 
+    [6, 22, 47], [7, 27, 40], [11, 18, 33], [2, 32, 35], [10, 28, 47], [5, 24, 41], [12, 25, 37], 
+    [3, 19, 39], [14, 31, 44], [16, 30, 34], [13, 20, 38], [9, 22, 36], [6, 17, 45], [4, 21, 42], 
+    [15, 29, 46], [8, 26, 43], [1, 23, 48], [1, 25, 42], [15, 22, 40], [8, 21, 41], [9, 18, 47], 
+    [6, 27, 43], [11, 30, 46], [7, 31, 35], [5, 20, 36], [14, 17, 38], [16, 28, 45], [4, 32, 37], 
+    [13, 23, 33], [12, 26, 44], [3, 29, 48], [2, 24, 39], [10, 19, 34]]
+
 def test_belief_prop():
-    hamming_code = np.array([[1,1,0,1,1,0,0],[1,0,1,1,0,1,0],[0,1,1,1,0,0,1]], dtype=np.uint32)
-    hamming_code_sparse = sparse.csr_matrix(hamming_code)
+    check_matrix = sparse.dok_matrix((48, 96), dtype=np.uint32)
+    for j, col in enumerate(_checks):
+        for i in col:
+            check_matrix[i-1,j] = 1
+    check_matrix = check_matrix.tocsr()
 
-    bp = BeliefPropagation(hamming_code_sparse)
+    bp = BeliefPropagation(check_matrix)
 
-    for i in range(hamming_code.shape[1]):
-        x = np.zeros(hamming_code.shape[1], dtype=np.uint32)
+    corrections = []
+    # 16-32 are weak bits. There isn't a good reason for them to be consecutive
+    for i in set(range(check_matrix.shape[1]))-set(range(16,32)):
+        x = np.zeros(check_matrix.shape[1], dtype=np.uint32)
         x[i] = 1
-        syndrome = (hamming_code @ x)%2
-        llr = -np.ones(hamming_code.shape[1])
-        correction = bp.decode(syndrome, llr, 30)
+        syndrome = (check_matrix @ x)%2
+        llr = -6*np.ones(check_matrix.shape[1])
+        correction = bp.decode(syndrome, llr, 60)
+        corrections.append((i, correction, x))
+
+    print([i for i,c,x in corrections if np.any(c != x)])
+
+    for i, correction, x in corrections:
         assert np.all(correction == x)
