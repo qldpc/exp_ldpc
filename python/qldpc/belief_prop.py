dbfca5744b03993d139cab006771d9ca8e954eaf
Chris Pattison <chpattison@gmail.com>
2022-10-08 12:55:22 -0700

    Start making stuff jit

--- python/qldpc/belief_prop.py
+++ python/qldpc/belief_prop.py
@@ -1,17 +1,29 @@
 import scipy.sparse as sparse
 import numpy as np
-from numba import int32, float64
+import numba
+from numba import njit
 from typing import Dict
 from array import array
 
+@njit
 def _log1pexp(x):
     '''Compute Log[1+Exp[x]] without overflowing in the exp.
     The discontinuity at 12e is about 6.8e-15'''
-    assert not np.isnan(x)
     
     transition = 12*np.e
     return x if x > transition else np.log1p(np.exp(x))
 
+@njit
+def _llr_sum(a_llr, b_llr):
+    '''Numerically stable way to compute the llr of (a + b). Equation (6) of Chen et al. IEEE Trans. Comm. 53 (8) 1288-1299 (2005)'''
+    ab_sum = np.abs(a_llr + b_llr)
+    ab_diff = np.abs(a_llr - b_llr)
+    
+    return (
+        np.sign(a_llr) * np.sign(b_llr) * np.minimum(np.abs(a_llr), np.abs(b_llr))
+        + _log1pexp(ab_sum)
+        - _log1pexp(ab_diff)
+    )
 class BeliefPropagation:
     _check_to_bit : Dict
     _bit_to_check : Dict
@@ -26,19 +38,6 @@ class BeliefPropagation:
             self._bit_to_check[j].append(i)
         
         self._check_matrix = check_matrix.tocsr()
-    
-
-    @staticmethod
-    def llr_sum(a_llr, b_llr):
-        '''Numerically stable way to compute the llr of (a + b). Equation (6) of Chen et al. IEEE Trans. Comm. 53 (8) 1288-1299 (2005)'''
-        ab_sum = np.abs(a_llr + b_llr)
-        ab_diff = np.abs(a_llr - b_llr)
-        
-        return (
-            np.sign(a_llr) * np.sign(b_llr) * np.minimum(np.abs(a_llr), np.abs(b_llr))
-            + _log1pexp(ab_sum)
-            - _log1pexp(ab_diff)
-        )
 
     def check_converged(self, llr, syndrome):
         return np.all((self._check_matrix @ np.where(llr > 0, 1, 0).astype(np.uint32))%2 == syndrome)
@@ -68,19 +67,19 @@ class BeliefPropagation:
                 f_llr = np.zeros(len(bit_list))
                 f_llr[0] = messages_v_to_c[check,bit_list[0]]
                 for i in range(1, len(bit_list)):
-                    f_llr[i] = self.llr_sum(f_llr[i-1], messages_v_to_c[check, bit_list[i]])
+                    f_llr[i] = _llr_sum(f_llr[i-1], messages_v_to_c[check, bit_list[i]])
 
                 # Backwards computation
                 b_llr = np.zeros(len(bit_list))
                 b_llr[-1] = messages_v_to_c[check, bit_list[-1]]
                 for i in range(len(bit_list)-2, -1, -1):
-                    b_llr[i] = self.llr_sum(b_llr[i+1], messages_v_to_c[check, bit_list[i]])
+                    b_llr[i] = _llr_sum(b_llr[i+1], messages_v_to_c[check, bit_list[i]])
 
                 # Compute messages
                 messages_c_to_v[check, bit_list[0]] = syndrome_R[check]*b_llr[1]
                 messages_c_to_v[check, bit_list[-1]] = syndrome_R[check]*f_llr[-2]
                 for i in range(2, len(bit_list)-1):
-                    messages_c_to_v[check, bit_list[i]] = syndrome_R[check]*self.llr_sum(f_llr[i-1], b_llr[i+1])
+                    messages_c_to_v[check, bit_list[i]] = syndrome_R[check]*_llr_sum(f_llr[i-1], b_llr[i+1])
                     
 
             # LLRs
