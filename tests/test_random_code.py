9db2844a4f266aa3a54403e321ca55f18dae3b39
Chris Pattison <chpattison@gmail.com>
2023-04-26 13:38:10 -0700

    Add flag to have generator full rank random matrices

--- tests/test_random_code.py
+++ tests/test_random_code.py
@@ -1,7 +1,16 @@
 from qldpc.random_code import random_check_matrix
+import numpy as np
 
 def test_random_check_matrix():
     r = 20
     n = 40
     H = random_check_matrix(r, n, seed=42)
     assert H.shape == (r,n)
+
+def test_random_check_matrix_full_rank():
+    r = 2
+    n = 5
+    for i in range(1000):
+        H = random_check_matrix(r, n, seed=i, full_rank=True)
+        assert np.linalg.matrix_rank(H) == min(r,n)
+
