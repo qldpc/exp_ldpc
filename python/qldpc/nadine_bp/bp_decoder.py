6175c7a9e10d058bafc03e2bd8013c1496eb99fe
Nadine Meister <nmeister@college.harvard.edu>
2023-07-19 13:04:08 -0700

    additions for outer code decoder

--- python/qldpc/nadine_bp/bp_decoder.py
+++ python/qldpc/nadine_bp/bp_decoder.py
@@ -6,6 +6,7 @@ import numpy as np
 from ldpc import bp_decoder
 from qldpc import SpacetimeCode
 from phi_distribution import *
+import time
 
 def decode_code(check_matrix, syndrome, prior, passin):
     # Decoder goes here
@@ -18,13 +19,10 @@ def decode_code(check_matrix, syndrome, prior, passin):
     return correction
 
 def calc_prob_odd_error(ps):
-    if len(ps) == 2:
-        p1 = ps[0]
-        p2 = ps[1]
-        return p1 * (1-p2) + (1-p1) * p2
-    else:
-        p1 = calc_prob_odd_error([ps[0], ps[1]])
-        return calc_prob_odd_error(np.append(p1, ps[2:]))
+    accumulator = 0
+    for i in range(len(ps)):
+        accumulator = accumulator + ps[i] - 2 * accumulator * ps[i]
+    return accumulator
 
 def run_simulation(samples, passin, code_path, d, p, **kwargs):
 
@@ -35,29 +33,44 @@ def run_simulation(samples, passin, code_path, d, p, **kwargs):
     spacetime_code = SpacetimeCode(code.checks.z, 10)
 
     results = []
+    # t00 = time.time()
 
     for _ in range(samples):
         # I hope 0 is no error, and 1 is error
-        error = []
-        prior = []
+        error = np.zeros(spacetime_code.spacetime_check_matrix.shape[1], dtype=np.uint32)
+        prior = np.zeros(spacetime_code.spacetime_check_matrix.shape[1], dtype=np.float64)
 
-        for _ in range(spacetime_code.spacetime_check_matrix.shape[1]):
+        # t_i = time.time()
+        for i in range(spacetime_code.spacetime_check_matrix.shape[1]):
             tau = round(3*np.sqrt(code.num_qubits)*d/10)
-            p_phi_taus = sample_phi(tau, d, p)
-            x_is = np.array([np.random.choice([0, 1], p=[1 - prob, prob]) for prob in p_phi_taus])
+            # t1 = time.time()
+            p_phi_taus = np.array(sample_phi(tau, d, p))
+            # t11 = time.time()
+            # x_is = np.array([np.random.choice([0, 1], p=[1 - prob, prob]) for prob in p_phi_taus])
+            x_is = np.where(1, 0, np.random.uniform(size=len(p_phi_taus)) <= p_phi_taus)
+            # t12 = time.time()
+            # print('sampling phi took', t11 - t1)
+            # print('generating x_i took', t12 - t11)
             error_i = np.sum(x_is) % 2
-            error.append(error_i)
+            error[i] = error_i
 
-            prior_i = calc_prob_odd_error(p_phi_taus)
-            prior.append(prior_i)
+            # t2 = time.time()
+            prior[i] = calc_prob_odd_error(p_phi_taus)
+            # t3 = time.time()
+            # print('calculating prob of odd error of', tau, 'probabilities: ', t3 - t2)
 
+        error = np.array(error)
+        # t_f = time.time()
+
+        # print('whole process of generating error took: ', t_f - t_i)
         # if not adding in idle measurements
         # llr_data, prior = sample_phi(spacetime_code.spacetime_check_matrix.shape[1], d, p)
         # error = np.array([np.random.choice([0, 1], p=[1-prob, prob]) for prob in prior])
 
         syndrome = (spacetime_code.spacetime_check_matrix @ error)%2 #syndrome in 01, all0s = all good
         correction = decode_code(spacetime_code.spacetime_check_matrix, syndrome, prior, passin) # a bunch of 0s when everything is correct
-
+        # t_f2 = time.time()
+        # print('running the decoder took: ', t_f2 - t_f)
         correction_single = spacetime_code.final_correction(correction)
         corrected_error = (spacetime_code.final_correction(error) + correction_single)%2
         logical_values = (code.logicals.z @ corrected_error)%2
@@ -66,6 +79,8 @@ def run_simulation(samples, passin, code_path, d, p, **kwargs):
 
     percentage_false = sum(value == False for value in results) / len(results) * 100
 
+    t_final = time.time()
+    # print('total time for 1 sample: ', t_final - t00)
     print('percentage logically corrected', percentage_false)
     return percentage_false
 
@@ -74,18 +89,16 @@ def main():
     # parser.add_argument('code_path', type=Path)
     # parser.add_argument('--samples', type=int, required=True, help='Number of samples to take')
     # parser.add_argument('--p_phys', type=float, required=True, help='physical error rate to sample at')
-    
-    
-    # args = parser.parse_args(sys.argv[1:])
 
+    # args = parser.parse_args(sys.argv[1:])
     # result = run_simulation(**vars(args))
 
     # stupider way to run this:
     code_path = PosixPath('hgp_1.qecc') #vars(args)['code_path']
     samples = 1 #vars(args)['samples']
 
-    p = 0.026
-    d = 7
+    p = 0.018
+    d = 15
     passin = True
     print(f'p = {p}, d = {d}, pass_input={passin}')
     results = []
--- python/qldpc/nadine_bp/phi_distribution.py
+++ python/qldpc/nadine_bp/phi_distribution.py
@@ -1,111 +1,19 @@
 from collections import defaultdict
-import ast
-import os
 import random
-import statsmodels.api as sm
 import numpy as np
+import time
+import json
 
-def extract_data(d, p, failure=True, faulty=False):
-    # Directory where the files are located
-    if faulty:
-        directories = ['/Users/nadinemeister/PycharmProjects/qsurface/tests/data_actualfaultymeas/',
-                     '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/data_cluster/',
-                     '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/data_cluster_transferred/',
-                     '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/data_cluster_higher_transferred/',
-                     '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/data_cluster_higher_transferred2/',
-                     '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/data_cluster_d25/',
-                     '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/d51_faulty/',
-                     '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/data_cluster_morehighertransfferred']
-    else:
-        directories = ['/Users/nadinemeister/PycharmProjects/qsurface/tests/data',
-                       '/Users/nadinemeister/PycharmProjects/qsurface/tests/data_LARGE',
-                       '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/data_cluster5199/',
-                       '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/d61_101_faultyFALSE/',
-                       '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/data_clusterfaultyFalse/',
-                       '/Users/nadinemeister/Dropbox/My Mac (Nadine’s MacBook Pro)/Documents/Harvard/Physics/Caltech/data_clusterPerfect/']
-    merged_data_failure = defaultdict(int)
+def load_phidistr(d, p, faulty=True):
+    filename = f'phi_distr/d_{d}_p_{p}_faultymeas.json'
 
-    if failure:
-        string = f"d{d}_failure_p_{p}_"
-    else:
-        string = f"d{d}_nofailure_p_{p}_"
+    with open(filename, "r") as file:
+        loaded_data = json.load(file)
 
-    # Iterate over the files in the directory
-    for directory in directories:
-        for file_name in os.listdir(directory):
-            if file_name.startswith(string):
-                file_path = os.path.join(directory, file_name)
-
-                with open(file_path, "r") as file:
-                    file_contents = file.read()
-
-                data_dict = ast.literal_eval(file_contents)
-
-                for key, value in data_dict.items():
-                    merged_data_failure.setdefault(key, 0)
-                    merged_data_failure[key] += value
-
-    return merged_data_failure
-
-
-def calc_problogfailure(d, p, faulty=False):
-    merged_data_nofailure = extract_data(d, p, failure=False, faulty=faulty)
-    merged_data_failure = extract_data(d, p, failure=True, faulty=faulty)
-
-    pr_log_failure = {}
-    for key in merged_data_nofailure:
-        sum_values = merged_data_nofailure[key] + merged_data_failure[key]
-        pr_log_failure[key] = merged_data_failure[key] / sum_values
-    return pr_log_failure
-
-def calc_phidistr(d, p, faulty=True):
-    merged_data_nofailure = extract_data(d, p, failure=False, faulty=faulty)
-    merged_data_failure = extract_data(d, p, failure=True, faulty=faulty)
-
-    # get slope
-    pr_log_failure = calc_problogfailure(d, p, faulty=faulty)
-    sorted_data = sorted(pr_log_failure.items(), key=lambda item: float(item[0]))
-    og_x = [float(item[0]) for item in sorted_data]
-    y = np.array([item[1] for item in sorted_data])
-    loggedy = np.log(y / (1 - y))
-    valid_indices = np.isfinite(loggedy)
-    x_valid = np.array(og_x)[valid_indices]
-    y_valid = np.array(loggedy)[valid_indices]
-    x = x_valid
-    y = y_valid
-    X = sm.add_constant(x)
-    model = sm.OLS(y, X)
-    results = model.fit()
-    intercept, slope = results.params
-
-    # make dict
-    distr = {}
-    for key in merged_data_nofailure:
-        sum_values = merged_data_nofailure[key] + merged_data_failure[key]
-        distr[float(key) * slope] = sum_values
-
-    return distr
+    return loaded_data
 
 def sample_phi(num_samples, d, p):
-    if d == 7 and p == 0.02: # to make sure it's slowed down by going through all the files
-        phi_distr = {-4.245923254463293: 1018006,
-                     -10.614808136158233: 1214966,
-                     -8.491846508926587: 1949122,
-                     -2.1229616272316467: 504618,
-                     -6.368884881694941: 1694989,
-                     -0.0: 133790,
-                     -12.737769763389881: 152383,
-                     -14.860731390621527: 2126}
-    elif d == 7 and p == 0.026:
-        phi_distr = {-3.5398031395840888: 27055,
-                     -0.0: 7098,
-                     -5.309704709376133: 29088,
-                     -7.0796062791681775: 21399,
-                     -1.7699015697920444: 17651,
-                     -8.84950784896022: 7268,
-                     -10.619409418752266: 441}
-    else:
-        phi_distr = calc_phidistr(d, p)
+    phi_distr = load_phidistr(d, p)
 
     keys = list(map(float, phi_distr.keys()))
     frequencies = list(phi_distr.values())
