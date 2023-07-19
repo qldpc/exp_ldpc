977f773e29627cc75123896ecb5a606e565ed5fd
Chris Pattison <chpattison@gmail.com>
2023-07-19 14:35:45 -0700

    Some optimizations

--- scripts/nadine_bp/bp_decoder.py
+++ scripts/nadine_bp/bp_decoder.py
@@ -6,23 +6,45 @@ import numpy as np
 from ldpc import bp_decoder
 from qldpc import SpacetimeCode
 from phi_distribution import *
-import time
+from time import time
+from numba import njit
 
-def decode_code(check_matrix, syndrome, prior, passin):
+def decode_code(check_matrix, syndrome, prior, passin, iterations):
     # Decoder goes here
-    if passin:
-        bp = bp_decoder(check_matrix, channel_probs=prior)
-    else:
-        bp = bp_decoder(check_matrix)
+    bp = bp_decoder(check_matrix, channel_probs=(prior if passin else None), max_iter=iterations)
 
+    t_i = time()
     correction = bp.decode(syndrome)
+    print('BP Runtime: ', time() - t_i)
+
     return correction
 
 def calc_prob_odd_error(ps):
-    accumulator = 0
+    accumulator = 0.0
     for i in range(len(ps)):
         accumulator = accumulator + ps[i] - 2 * accumulator * ps[i]
     return accumulator
+
+@njit
+def sample_error_prior_kernel(rng, p_phi_tau):
+    error = np.zeros(p_phi_tau.shape[0], dtype=np.uint32)
+    prior = np.zeros(p_phi_tau.shape[0], dtype=np.float64)
+
+    for i in range(p_phi_tau.shape[0]):
+        mask = np.random.uniform(0.0, 1.0, p_phi_tau.shape[1]) <= p_phi_tau[i,:]
+        x_is = np.where(mask, 1, 0)
+        error_i = np.sum(x_is) % 2
+        error[i] = error_i
+        prior[i] = calc_prob_odd_error(p_phi_tau[i,:])
+    return error, prior
+
+def sample_error_prior(rng, size, tau, phi_distr):
+    # I hope 0 is no error, and 1 is error
+    
+    p_phi_tau = sample_phi(rng, (size,tau), phi_distr)
+    error, prior = sample_error_prior_kernel(rng, p_phi_tau)
+        
+    return error, prior
 
 def run_simulation(samples, passin, code_path, d, p, **kwargs):
 
@@ -32,45 +54,26 @@ def run_simulation(samples, passin, code_path, d, p, **kwargs):
     # doing arbitrary 10 rounds of stabilizer measurements for now
     spacetime_code = SpacetimeCode(code.checks.z, 10)
 
+    rng = np.random.default_rng()
+    
     results = []
     # t00 = time.time()
 
+    phi_distr = get_phidistr(d, p)
+    tau = int(np.ceil(3*np.sqrt(code.num_qubits)*d/10.0))
     for _ in range(samples):
-        # I hope 0 is no error, and 1 is error
-        error = np.zeros(spacetime_code.spacetime_check_matrix.shape[1], dtype=np.uint32)
-        prior = np.zeros(spacetime_code.spacetime_check_matrix.shape[1], dtype=np.float64)
-
-        # t_i = time.time()
-        for i in range(spacetime_code.spacetime_check_matrix.shape[1]):
-            tau = round(3*np.sqrt(code.num_qubits)*d/10)
-            # t1 = time.time()
-            p_phi_taus = np.array(sample_phi(tau, d, p))
-            # t11 = time.time()
-            # x_is = np.array([np.random.choice([0, 1], p=[1 - prob, prob]) for prob in p_phi_taus])
-            x_is = np.where(1, 0, np.random.uniform(size=len(p_phi_taus)) <= p_phi_taus)
-            # t12 = time.time()
-            # print('sampling phi took', t11 - t1)
-            # print('generating x_i took', t12 - t11)
-            error_i = np.sum(x_is) % 2
-            error[i] = error_i
-
-            # t2 = time.time()
-            prior[i] = calc_prob_odd_error(p_phi_taus)
-            # t3 = time.time()
-            # print('calculating prob of odd error of', tau, 'probabilities: ', t3 - t2)
-
+        t_i = time()
+        error, prior = sample_error_prior(rng, spacetime_code.spacetime_check_matrix.shape[1], tau, phi_distr)
         error = np.array(error)
-        # t_f = time.time()
 
-        # print('whole process of generating error took: ', t_f - t_i)
+        print('whole process of generating error took: ', time() - t_i)
         # if not adding in idle measurements
         # llr_data, prior = sample_phi(spacetime_code.spacetime_check_matrix.shape[1], d, p)
         # error = np.array([np.random.choice([0, 1], p=[1-prob, prob]) for prob in prior])
 
         syndrome = (spacetime_code.spacetime_check_matrix @ error)%2 #syndrome in 01, all0s = all good
-        correction = decode_code(spacetime_code.spacetime_check_matrix, syndrome, prior, passin) # a bunch of 0s when everything is correct
-        # t_f2 = time.time()
-        # print('running the decoder took: ', t_f2 - t_f)
+        correction = decode_code(spacetime_code.spacetime_check_matrix, syndrome, prior, passin, iterations=code.num_qubits) # a bunch of 0s when everything is correct
+
         correction_single = spacetime_code.final_correction(correction)
         corrected_error = (spacetime_code.final_correction(error) + correction_single)%2
         logical_values = (code.logicals.z @ corrected_error)%2
@@ -79,8 +82,6 @@ def run_simulation(samples, passin, code_path, d, p, **kwargs):
 
     percentage_false = sum(value == False for value in results) / len(results) * 100
 
-    t_final = time.time()
-    # print('total time for 1 sample: ', t_final - t00)
     print('percentage logically corrected', percentage_false)
     return percentage_false
 
--- scripts/nadine_bp/phi_distribution.py
+++ scripts/nadine_bp/phi_distribution.py
@@ -3,26 +3,28 @@ import random
 import numpy as np
 import time
 import json
+from functools import lru_cache
+from numba import njit,jit
 
-def load_phidistr(d, p, faulty=True):
+def get_phidistr(d, p, faulty=True):
     filename = f'phi_distr/d_{d}_p_{p}_faultymeas.json'
 
-    with open(filename, "r") as file:
-        loaded_data = json.load(file)
+    with open(filename, "r") as data_file:
+        phi_distr = json.load(data_file)
 
-    return loaded_data
-
-def sample_phi(num_samples, d, p):
-    phi_distr = load_phidistr(d, p)
-
-    keys = list(map(float, phi_distr.keys()))
-    frequencies = list(phi_distr.values())
+    keys = np.array([float(x) for x in iter(phi_distr.keys())])
+    frequencies = np.array(list(phi_distr.values()), dtype=np.float)
 
     # Normalize the frequencies to ensure they sum up to 1
-    total_frequency = sum(frequencies)
-    normalized_frequencies = [f / total_frequency for f in frequencies]
+    total_frequency = np.sum(frequencies)
+    normalized_frequencies = frequencies / total_frequency
+
+    return keys, normalized_frequencies
+
+def sample_phi(rng, size, phi_distr):
+    phi_values, phi_freq = phi_distr
 
     # Sample from the distribution
-    sample_cphi = random.choices(keys, weights=normalized_frequencies, k=num_samples)
+    sample_cphi = rng.choice(phi_values, p=phi_freq, size=size)
     sample_q = np.exp(sample_cphi) / (1 + np.exp(sample_cphi))
     return sample_q
