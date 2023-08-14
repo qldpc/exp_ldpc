dccf4c111449f833cb5b27778c52a43e3e40ff64
Chris Pattison <chpattison@gmail.com>
2023-08-13 19:16:44 -0700

    more WIP quasicyclic LP codes

--- python/qldpc/quasicyclic_lifted_product_code.py
+++ python/qldpc/quasicyclic_lifted_product_code.py
@@ -6,19 +6,16 @@ import scipy.linalg as linalg
 import scipy.sparse as sparse
 import warnings
 
-def circulant_generator(k, circulant_size):
-    '''returns the l by l circulant matrix that implents the shift to the right by k positions'''
-    return linalg.circulant([1 if i == k else 0 for i in range(circulant_size)])
-
-def shifts_to_blocks(a, circulant_size):
-    '''Given a matrix of shifts (powers of x) represented as integers, returns the full block matrix suitable for passing into the code generator
+def shifts_to_polynomials(a):
+    '''Given a matrix of shifts (powers of x) represented as integers, returns a matrix of polynomails suitable for passing to the code generator
     '''
-    pass
+    def scalar_to_poly(s):
+        return Poly.Degrees([s])
+    return np.vectorize(scalar_to_poly(s))(a)
 
-def quasicyclic_lifted_product_code(quasicyclic_check_matrix, circulant_size, check_complex=None, compute_logicals=None) -> QuantumCode:
+def quasicyclic_lifted_product_code(quasicyclic_check_matrix, l, check_complex=None, compute_logicals=None) -> QuantumCode:
     '''
-    The input matrix is an n x m quasicyclic check matrix with elements in the polynomial ring GF2[x]/(x^l-1) represented as l by l permutation matrices over GF2.
-    This is represented as an n x m x l x l tensor A_abcd in GF2 where the i,j coefficient (block) of the matrix is the lxl matrix with cd coefficient equal to A_ijcd
+    The input matrix is an n x m quasicyclic check matrix with elements in the polynomial ring GF2[x]/(x^l-1)
     '''
     
     if check_complex is None:
@@ -27,38 +24,38 @@ def quasicyclic_lifted_product_code(quasicyclic_check_matrix, circulant_size, ch
     if compute_logicals is None:
         compute_logicals = False
 
+    def circulant_reduce(a):
+        return a % Poly.Degrees([l,0])
+        
+    def identity(size):
+        return numpy.identity(size, dtype=np.uinp32)*Poly.One()
+        
+    def antipode(a):
+        return Poly((l-coeffs)%l)
+
+    def poly_to_matrix(a):
+        return linalg.circulant(a.coefficients(size=l, order='asc').astype(np.uint32))
     
-    # def block_repr(a):
-    #     return linalg.circulant(a.cofficients(size=circulant_size, order='asc'))
+    def embed_binary_matrix(a):
+        a_reduced = circulant_reduce(a)
+        return np.block(np.vectorize(poly_to_matrix)(a_reduced))
+    
 
-    partial_A = quasicyclic_check_matrix.astype(np.uint32)
-    partial_B = np.einsum('abcd->badc', quasicyclic_check_matrix).astype(np.uint32)
-
-    # Takes two matrices with the block structure and returns the kronecker product with block structure forgotten
-    def kronecker_embed(a, b):
-        # Indices left to right are A, B, circulant repp
-        assert a.shape[2] == circulant_size
-        assert b.shape[2] == circulant_size
-        product = np.einsum('abcd,efdh->aecbfh')%2
-        return np.reshape(product, (a.shape[0]*b.shape[0]*circulant_size, a.shape[1]*b.shape[1]*circulant_size), order='F')
-
-    def circulant_identity(size):
-        inner_identity = np.identity(circulant_size, dtype=np.uint32)
-        outer_identity = np.identity(size, dtype=np.uint32)
-        return np.einsum('a,b->ab',outer_identity, inner_identity)
+    partial_A = quasicyclic_check_matrix
+    partial_B = np.vectorize(antipode)(np.transpose(quasicyclic_check_matrix))
 
     # D^A x I + I x D^B : A_1 x B_1 -> A_0 x B_1 + A_1 x B_0
-    partial_2 = np.vstack([
-        kronecker_embed(partial_A, circulant_identity(partial_B.shape[1])),
-        kronecker_embed(circulant_identity(partial_A.shape[1]), partial_B)
-    ]).astype(np.uint32)
+    partial_2 = embed_binary_matrix(np.vstack([
+        np.kron(partial_A, identity(partial_B.shape[1])),
+        np.kron(identity(partial_A.shape[1]), partial_B)
+    ]))
     
     partial_1_factors = [
-        sparse.kron(circulant_identity(partial_A.shape[0]), partial_B),
-        sparse.kron(partial_A, circulant_identity(partial_B.shape[0]))
+        np.kron(identity(partial_A.shape[0]), partial_B),
+        np.kron(partial_A, identity(partial_B.shape[0]))
     ]
 
-    partial_1 = np.hstack(partial_1_factors).astype(np.uint32)
+    partial_1 = embed_binary_matrix(np.hstack(partial_1_factors))
 
     if check_complex:
         assert np.all((partial_1 @ partial_2).data % 2 == 0)
